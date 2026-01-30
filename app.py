56ae8f670f422f3527c99fb2790b438dfb22deca
shaik sameer <sksameer9452@gmail.com>
2026-01-30 12:52:25 +0530

    add the create route

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+# Create page
+@app.route("/create")
+def create():
+    return render_template("create.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
