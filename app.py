9620de8fa45374349e0abe25e05754d93fd6d9f1
shaik sameer <sksameer9452@gmail.com>
2026-01-31 15:04:04 +0530

    add the preview route

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ def home():
 @app.route("/create")
 def create():
     return render_template("create.html")
+# preview page
+@app.route("/preview")
+def preview():
+    return render_template("preview.html")      
 
 if __name__ == "__main__":
     app.run(debug=True)
