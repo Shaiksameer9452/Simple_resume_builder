b5f1579afe259c1c8bdcd04ef2bba7f34ca6a622
shaik sameer <sksameer9452@gmail.com>
2026-02-13 12:16:19 +0530

    modify the content

--- app.py
+++ app.py
@@ -10,12 +10,10 @@ def get_db():
     conn.row_factory = sqlite3.Row  
     return conn
 
-
 # HOME PAGE 
 @app.route("/")
 def home():
     return render_template("home.html")
-
 
 # CREATE RESUME 
 @app.route("/create", methods=["GET", "POST"])
@@ -50,11 +48,22 @@ def create():
         resume = cursor.fetchone()
 
         if resume:
+            #  Get old projects
+            cursor.execute("SELECT projects FROM resumes WHERE user_id = ?", (user_id,))
+            old_data = cursor.fetchone()
+
+            old_projects = old_data["projects"] if old_data and old_data["projects"] else ""
+
+            #  Combine old + new projects
+            if old_projects.strip():
+                updated_projects = old_projects + "\n" + projects
+            else:
+                updated_projects = projects
             cursor.execute("""
                 UPDATE resumes
                 SET phone = ?, education = ?, skills = ?, projects = ?
                 WHERE user_id = ?
-            """, (phone, education, skills, projects, user_id))
+            """, (phone, education, skills, updated_projects, user_id))
             resume_id = resume["id"]
         else:
             cursor.execute("""
@@ -62,21 +71,16 @@ def create():
                 VALUES (?, ?, ?, ?, ?, ?)
             """, (user_id, phone, education, skills, projects, created_at))
             resume_id = cursor.lastrowid
-
         db.commit()
         db.close()
-
         return redirect(url_for("preview", id=resume_id))
-
     return render_template("create.html")
-
 
 # PREVIEW RESUME 
 @app.route("/preview/<int:id>")
 def preview(id):
     db = get_db()
     cursor = db.cursor()
-
     cursor.execute("""
         SELECT users.name, users.email,
                resumes.phone, resumes.education,
@@ -85,12 +89,11 @@ def preview(id):
         JOIN users ON resumes.user_id = users.id
         WHERE resumes.id = ?
     """, (id,))
-
     resume = cursor.fetchone()
     db.close()
-
+    if not resume:
+        return "Resume not found", 404
     return render_template("preview.html", resume=resume)
-
 
 # RUN APP 
 if __name__ == "__main__":
